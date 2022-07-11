1ac60c7798d2ddde35c65339c6ce6e0d41c98f78
xiaohe741 <xiaohe741@sina.com>
2022-07-11 11:44:33 +0800

    add folder static

---
+++ static/image.js
@@ -0,0 +1,19 @@
+/**
+ * +-------------------------------------------------
+ * |    Static / Images.js
+ * +-------------------------------------------------
+ * |    Charlie / xiaohe741@sina.com
+ * +-------------------------------------------------
+ */
+
+export const avatar = [
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/0c300392-ee01-4a3a-8aa5-1f8ce65bc998.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/52d7d301-aedd-45bb-adb9-6a3f0981196f.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/40fac287-d159-493c-a29f-88125f6882f5.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/b7341683-22f8-4887-b842-99ab1a7274d3.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/f21c4058-88f7-448a-8730-456fa68e83ef.jpg',
+];
+
+export default {
+	avatar,
+}
